47444ffadee11d1a97230bc8529ec8c89d779abe
YaYa <2678288233@qq.com>
2019-12-02 11:50:27 +0800

    fix name

--- bat/restAPI.bat
+++ bat/restAPI.bat
@@ -8,6 +8,6 @@ curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoke
 
 curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoker_war/login/login -d "{username:1111,password:1111}"
 
-curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoker_war/userInfo/detail -d "{id:1111}"
+curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoker_war/userInfo/detail -d "{id:15749232868170}"
 
-curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoker_war/userInfo/basic -d "{id:1111}"
+curl -X POST -H "Content-Type: application/json" http://localhost:80/ChinesePoker_war/userInfo/basic -d "{id:15749232868170}"
